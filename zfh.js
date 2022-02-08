4862330119d690e3f98ec20c80d7354dfb7b01c8
xingkong <linmcqq2020@qq.com>
2022-02-08 15:40:21 +0800

    Create zfh.js

---
+++ zfh.js
@@ -0,0 +1,83 @@
+  var res = `
+                                                                                                      
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                               .,,,:                                                
+                                           :D#WWWWWWWWWWE:                                          
+                                        iWWWWWWWWWWWWWKKKEEE.                                       
+                                      iWWWWWWWWWWWWWWWKKKEKKKW,                                     
+                                    j#WWWWWWWWWWWWWWWWKKKKKKKKKE                                    
+                            .j:.;L;iWWWWWWWWWWWWWWWWWWKKKKKKKKKKW                                   
+                                  LWWWWWWWWWWWWWWWWWWWKWWKKKKKKWWW:                                 
+                                 ;WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW                                 
+                                .WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW#                                
+                                #WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWK                               
+                               fWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW                               
+                               WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWE                              
+                              DWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW.                             
+                              WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWD                             
+                             :WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW#                             
+                             DWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW                             
+                             WKWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW.                            
+                             KWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW;                            
+                            .WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWj                            
+                            .WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWE                            
+                            tWWWKWWWWWWWWWWWWKWWWWWWWWWWWWWWWWWWWWWWWWWK                            
+                            GKKKWWWWWWWWWWWWWDWWWWWWWWWWWWWWWWWWWWWWWWWK                            
+                            GKKKKKWWWWWWWWWWWWDWWWWWWWWWWWWWWWWWWWWWWWWE                            
+                            GEEEEEKKLWWWWWWWWE;WWWWWWWWWWWWWWWWWWWWWWWWj                            
+                            GEEEEEEKiWWWWWKKKKD:WWWWWWWWWWWWWWWWWWWWWWW.                            
+                            GDEEEEEKtGKWKKKKKKL GWWWWWWWWWWWWWWWWWWWWWW                             
+                            tiEEEEEEK.WKKKEEKEii WWKWWKWWWWWWWWWWWWWWW#                             
+                            :,EEEEDEE :KKKEEEDfi :WLWKDWKKWWWWE WWWWWWL                             
+                             tLDDDDEE  DKEEDDDD ; fWKKEDEKKKWWD;.WWWWW:                             
+                   i         GtDDDDDDt  KEEDDDD .EKWf,E:EEKKKKDG;WWWW,                              
+                   ;i        E DDDDDD...GKD;DDD, .  W.;tEEEKKfj KWWWKf                              
+                      j      , jDGDDD:,.:;EDjDDL     j LGEEEK tEKKKi.,                              
+                      ;E      , DGGDDDDKE..Di;D,       .:EEGfKKKKGK,                                
+                         L    t  GGGGt;    .:  .         DED:KKKWE.                                 
+                         L       GGGGf,.    .           .DGfKKKK;K                                  
+                                 iGGGG:                 iD fKKtK t                                  
+                                  tGjGj                 j.tW.K.i                                    
+                                   DGDG                  WK. .                                      
+                                   .t t:       ;       jj;Li;Gi                                     
+                                    t  j,            jtii;;;,;E                                     
+                                         ij,     :jDE;,,,,;jt;E                                     
+                                          ,jfDDDLDL:,,,,,,,,;fE                                     
+                                         :;;,,,,;,,,,,,,,,,;;;;G                                    
+                                          ;,,,,,i,,,,,,,,,,;;;;;                                    
+                                          ;,,,,;,,,,,,,,,,,;;;;f                                    
+                                          ,,,,,,j,,,Dt,,,;itjfEK                                    
+                                          i,,,i;i,,,#,,,,,;;;;;i,                                   
+                                          t;:L,i,,,,K,,,;;;;;;;LD                                   
+                                         t;;t,,i,,,,G,,,;;;;;iiii                                   
+                                         L;;j;;t;,;;;,,;;;;itiiitG                                  
+                                        ,iiG;;;t;;;f,,;;;;;;tiiift                                  
+                                       :jiGi;;i;;;;W,;;;;;;iiiiiii.                                 
+                                      jfiti;i;j;;;;D;;;;;iiLiiiiiiL                                 
+                                    tifiiEiiitiGGttf;i;;iiitiijiiiG                                 
+                                  :fiLiitjiiGL;i;;ffiiiiiiiiiijiiti                                 
+                                 iiiLiiiftG;tiiiiLGtiiiiiiiiijiiitij                                
+                                ;tijfiiiWtttjtfjiiftiiiiiijttiiiiiiL                                
+                                GGiiiiiiiitjiiiiiiftiiiiiiGtiiiiiifL.                               
+                               D;tiiiiiiifiiiiiiiiiiiiiiiiDiiiiiiiiiE                               
+                              LtiiiiiiiifiiiiiiiiiiiiiiiiiGiiiiiiiift                               
+                              DttttiiiiLiiiiiiiiiiiiiiiiijiiiiiiiiiiit                              
+                              KttttitiftiiiiiiiiiiiiiijitKfjiiiiiiiiiE                              
+                             jtDitttttiitiiiiiiiiiiiiijWGiiiiiiiiiiiiL                              
+                       j     EttfttttLttttiiiiiiiiiiiijitiiiiiiiiiijij                              
+                       iL  GWW#tttttttttttiiiiiiiiiiiiiifiiiiiiiiiifit                              
+                       :DDEKKWWWtttttttttttiiiitiiiiiiititiiiiiiiifiif                              
+                        EEEKKWWWGtfttttttitDjWW#tjiiiiiitfiiiiiittiii,                              
+                        KKEKKWWWWDiLKtjffiiiWWWWWtttiiiitiLDGfLLiiij,                               
+                        KWWKWWWWKKKLttttttiWWWWWWWttiiiiijGiiiiiitL     : .                         
+                        ,.           :jGWWWKKKKKWWWiiiiiiijWKKEKt  DKE,KEG                          
+                                           WKKKKWWWiKi;iG#,        j:f.j                            
+                                           KEEKKKWWEEDt                                             
+                                           EEEEEK,                                                  
+                                           EKK;                                                     
+                                           L,                                                       
+                                                                                                    
+                     `
+  console.log(res)
